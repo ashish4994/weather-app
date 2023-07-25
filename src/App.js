186d7b9014656b15bf81e28851c0899f96db1910
Ashok Thota <ashok.chakravarthy.thota@intel.com>
2023-07-25 15:32:37 -0700

    Added font awesome library

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Header from './components/header.component'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Home from './components/Home.component';
-import ToDo
- from './components/ToDo.component';
+import ToDo from './components/ToDo/ToDo.component';
 function App() {
 
   return (
